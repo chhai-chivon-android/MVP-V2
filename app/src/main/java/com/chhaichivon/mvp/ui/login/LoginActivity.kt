61d48e64a3530376cbbbcc6377228be6c975dd5e
Chhai Chivon <chivonchhai.it@gmail.com>
2021-02-18 17:05:35 +0700

    Add LoginContract

--- app/src/main/java/com/chhaichivon/mvp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/chhaichivon/mvp/ui/login/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.chhaichivon.mvp.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import butterknife.BindView
+import butterknife.ButterKnife
 import com.chhaichivon.mvp.R
+import com.chhaichivon.mvp.ui.post.PostActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() , View.OnClickListener{
 
     @BindView(R.id.et_username) lateinit var mUsernameEditText : EditText
     @BindView(R.id.et_password) lateinit var mPasswordEditText : EditText
@@ -16,5 +21,22 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        ButterKnife.bind(this);
+        mLoginButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v){
+            mLoginButton -> login()
+        }
+    }
+
+    fun login(){
+        Log.d("mUsernameEditText : ", mUsernameEditText.text.toString())
+        println()
+        Log.d("mPasswordEditText : ", mPasswordEditText.text.toString())
+        val intent = Intent(this, PostActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/chhaichivon/mvp/ui/post/PostActivity.kt
+++ app/src/main/java/com/chhaichivon/mvp/ui/post/PostActivity.kt
@@ -1,9 +1,18 @@
 package com.chhaichivon.mvp.ui.post;
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import butterknife.ButterKnife
 import com.chhaichivon.mvp.R
+
 
 class PostActivity : AppCompatActivity() {
 
@@ -11,5 +20,42 @@ class PostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         ButterKnife.bind(this)
+
+        val actionBar = supportActionBar
+        val tv = TextView(applicationContext)
+        val typeface = Typeface.createFromAsset(assets, "font/Hanuman-Bold.ttf")
+        val lp = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT)
+        tv.layoutParams = lp
+        tv.text = "អត្ថបទ"
+        tv.textSize = 25f
+        tv.setTextColor(Color.WHITE)
+        tv.setTypeface(typeface, Typeface.ITALIC)
+        if (actionBar != null) {
+            actionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+            actionBar.customView = tv
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.post_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        if (id == R.id.action_one) {
+            Toast.makeText(this, "Item One Clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
+        if (id == R.id.action_two) {
+            Toast.makeText(this, "Item Two Clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
+        if (id == R.id.action_three) {
+            Toast.makeText(this, "Item Three Clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
